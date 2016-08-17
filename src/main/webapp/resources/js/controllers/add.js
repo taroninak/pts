c9076992a9b8ca897b1e2b5989191d011b844ef8
taronpa <taron.petrosyan@picsart.com>
2016-08-17 17:18:16 +0400

    Implement save cancel flow

--- src/main/webapp/resources/js/controllers/add.js
+++ src/main/webapp/resources/js/controllers/add.js
@@ -1,24 +1,45 @@
-angular.module("projectApp").controller('addCtrl', function($scope, $http, $route) {
+angular.module("projectApp").controller('addCtrl', function ($scope, $http, $route) {
+
+    $scope.title = 'Project Add Form';
+    $scope.project = {};
+    $scope.contacts = [];
+    $scope.statuses = ["--Select Status--", "NEW", "ACTIVE", "ONGOING", "CLOSED"];
+    $scope.project.status = "--Select Status--";
+
     $scope.removeContact = function (contactId) {
         console.log("click");
-        $scope.contacts = $scope.contacts.filter(function(contact) {
+        $scope.contacts = $scope.contacts.filter(function (contact) {
             return (contact.id == contactId) ? false : true;
         });
     };
 
     $scope.cancel = function () {
-
+        window.history.back();
     };
 
     $scope.save = function () {
-        for(var index in $scope.contacts) {
-            $http.post("/api/contacts", JSON.stringify($scope.contacts[index]))
-            .success(function() {
-
-            });
+        if($scope.project.status == "--Select Status--" || !$scope.project.title) {
+            return alert("Project's Title and Status should be specified!");
         }
+
+        ///TODO validate contacts list
+
+        $http.post("/api/projects", JSON.stringify($scope.project)).then(function (result) {
+            if(!$scope.contacts || !$scope.contacts.length) return window.history.back();
+            for (var index in $scope.contacts) {
+                $scope.contacts[index].projectId = result.dtata.id;
+                if(!$scope.contacts[index].phone) {
+                    $scope.contacts[index].phone = '';
+                }
+                $http.post("/api/contacts", JSON.stringify($scope.contacts[index]));
+            }
+            window.history.back();
+        });
     };
 
-    $scope.statuses = ["--Select Status--", "NEW", "ACTIVE", "ONGOING", "CLOSED"];
-    $scope.selectedStatus = "--Select Status--";
+
+
+    $scope.add = function () {
+        $scope.contacts.push({});
+    }
 });
--- src/main/webapp/resources/js/controllers/edit.js
+++ src/main/webapp/resources/js/controllers/edit.js
@@ -1,10 +1,23 @@
-angular.module("projectApp").controller('editCtrl', function($scope, $http, $route, $routeParams) {
+angular.module("projectApp").controller('editCtrl', function ($scope, $http, $route, $routeParams) {
+
+    $scope.title = 'Project Edit Form';
     var id = $route.current.params.id;
     $scope.removedContacts = [];
+
+    $http.get("/api/projects/" + id).then(function (result) {
+        $scope.statuses = ["--Select Status--", "NEW", "ACTIVE", "ONGOING", "CLOSED"];
+        $scope.project = result.data ? result.data : {status: "--Select Status--"};
+
+        $http.get("/api/projects/" + id + "/contacts").then(function (result) {
+            $scope.contacts = result.data ? result.data : [];
+        });
+
+    });
+
+
     $scope.removeContact = function (contactId) {
-        console.log("click");
-        $scope.contacts = $scope.contacts.filter(function(contact) {
-            if(contact.id == contactId) {
+        $scope.contacts = $scope.contacts.filter(function (contact) {
+            if (contact.id == contactId) {
                 $scope.removedContacts.push(contact);
                 return false;
             }
@@ -13,33 +26,44 @@ angular.module("projectApp").controller('editCtrl', function($scope, $http, $rou
     };
 
     $scope.cancel = function () {
-        [].push.apply($scope.contacts, $scope.removedContacts);
-        $scope.removedContacts = [];
+        window.history.back();
+        //[].push.apply($scope.contacts, $scope.removedContacts);
+        //$scope.removedContacts = [];
     };
 
     $scope.save = function () {
-        for(var index in $scope.removedContacts) {
-            $http.delete("/api/contacts/" + $scope.removedContacts[index].id).success(function () {
-                for(var index in $scope.contacts) {
-                    $http.put("/api/contacts", JSON.stringify($scope.contacts[index])).success(function() {
+        if($scope.project.status == "--Select Status--" || !$scope.project.title) {
+            return alert("Project's Title and Status should be specified!");
+        }
 
+        ///TODO validate contacts list
+
+        $http.patch("/api/projects/" + $scope.project.id, JSON.stringify($scope.project)).then(function (result) {
+            for (var index in $scope.removedContacts) {
+                $http.delete("/api/contacts/" + $scope.removedContacts[index].id).then(function () {
+                    
+                });
+            }
+            for (var index in $scope.contacts) {
+                $scope.contacts[index].projectId = $scope.project.id;
+                if(!$scope.contacts[index].phone) {
+                    $scope.contacts[index].phone = '';
+                }
+                if ($scope.contacts[index].id) {
+                    $http.patch("/api/contacts/" + $scope.contacts[index].id, JSON.stringify($scope.contacts[index])).then(function (result) {
+                        console.log(result);
+                    });
+                } else {
+                    $http.post("/api/contacts", JSON.stringify($scope.contacts[index])).then(function () {
+                        
                     });
                 }
-            });
-        }
+            }
+            window.history.back();
+        });
     };
 
-    $http.get("/api/projects/"+id).then(function(result) {
-        $scope.statuses = ["--Select Status--", "NEW", "ACTIVE", "ONGOING", "CLOSED"];
-        $scope.project = result.data;
-        if($scope.project.status) {
-            $scope.selectedStatus = $scope.project.status;
-        } else {
-            $scope.selectedStatus = "--Select Status--";
-        }
-        $http.get("/api/projects/" + id + "/contacts").then(function(result) {
-            $scope.contacts = result.data;
-        });
-
-    });
+    $scope.add = function () {
+        $scope.contacts.push({});
+    }
 });
