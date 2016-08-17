3de4f80d765b46c638ebebb126380674f80ea208
taronpa <taron.petrosyan@picsart.com>
2016-08-17 13:59:23 +0400

    Add missing http request handling, make api RESTfull

--- src/main/java/com/pts/controller/ApiController.java
+++ src/main/java/com/pts/controller/ApiController.java
@@ -1,15 +1,20 @@
 package com.pts.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pts.model.Contact;
+import com.pts.model.Project;
+import com.pts.model.ProjectStatus;
 import com.pts.service.ContactService;
 import com.pts.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 /**
  * Created by taronpetrosyan on 8/16/16.
@@ -28,73 +33,155 @@ public class ApiController {
     @SuppressWarnings("SameReturnValue")
     @RequestMapping(value = "/api/projects", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
     @ResponseBody
-    public String getProjects() {
+    public ResponseEntity<String> listProjects() {
         String res;
         try{
-            res = objectMapper.writeValueAsString(projectService.listProjects());
+            return ResponseEntity.ok(objectMapper.writeValueAsString(projectService.listProjects()));
         }catch(JsonProcessingException ex) {
-            res = "{ err: " + ex.getMessage() + "}";
+            ///TODO log exception
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
-        return res;
     }
 
     @SuppressWarnings("SameReturnValue")
     @RequestMapping(value = "/api/projects/{id}", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
     @ResponseBody
-    public String getProjectById(@PathVariable("id") int id) {
-        String res;
+    public ResponseEntity<String> getProject(@PathVariable("id") int id) {
         try{
-            res = objectMapper.writeValueAsString(projectService.getProjectById(id));
+            return ResponseEntity.ok(objectMapper.writeValueAsString(projectService.getProjectById(id)));
         }catch(JsonProcessingException ex) {
-            res = "{ err: " + ex.getMessage() + "}";
+            ///TODO log exception
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
-        return res;
+    }
+
+    @SuppressWarnings("SameReturnValue")
+    @RequestMapping(value = "/api/projects", method = RequestMethod.POST, produces = "application/json; charset=utf-8")
+    @ResponseBody
+    public ResponseEntity<String> postProject(@RequestBody String json) {
+        Project project;
+        try{
+            JsonNode node = objectMapper.readTree(json);
+            if(!node.has("title") || !node.has("status")) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+            }
+            project = new Project(null, node.get("title").asText(), ProjectStatus.valueOf(node.get("status").asText()));
+        }catch(Exception ex) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+        projectService.addProject(project);
+        return ResponseEntity.status(HttpStatus.CREATED).location(URI.create("/api/projects")).body(null);
+    }
+
+    @SuppressWarnings("SameReturnValue")
+    @RequestMapping(value = "/api/projects/{id}", method = RequestMethod.PATCH, produces = "application/json; charset=utf-8")
+    @ResponseBody
+    public ResponseEntity<String> patchProject(@PathVariable("id") int id, @RequestBody String json) {
+        Project project;
+        try{
+            JsonNode node = objectMapper.readTree(json);
+            if(!node.has("title") || !node.has("status")) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+            }
+            project = new Project(id, node.get("title").asText(), ProjectStatus.valueOf(node.get("status").asText()));
+        }catch(Exception ex) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+        projectService.editProject(project);
+        return ResponseEntity.ok(json);
     }
 
     @SuppressWarnings("SameReturnValue")
     @RequestMapping(value = "/api/projects/{id}", method = RequestMethod.DELETE, produces = "application/json; charset=utf-8")
     @ResponseBody
-    public String deleteProjectById(@PathVariable("id") int id) {
+    public ResponseEntity<String> deleteProject(@PathVariable("id") int id) {
         projectService.deleteProject(id);
-        return "{ \"success\": " + "\"Project with '" + id + "' has been deleted.\"}";
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(null);
     }
 
     @SuppressWarnings("SameReturnValue")
     @RequestMapping(value = "/api/projects/{id}/contacts", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
     @ResponseBody
-    public String getProjectContacts(@PathVariable("id") int id) {
-        String res;
+    public ResponseEntity<String> listProjectContacts(@PathVariable("id") int id) {
         try{
-            res = objectMapper.writeValueAsString(contactService.listProjectContacts(id));
+            return ResponseEntity.ok(objectMapper.writeValueAsString(contactService.listProjectContacts(id)));
         }catch(JsonProcessingException ex) {
-            res = "{ err: " + ex.getMessage() + "}";
+            ///TODO log exception
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
-        return res;
     }
 
     @SuppressWarnings("SameReturnValue")
     @RequestMapping(value = "/api/contacts", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
     @ResponseBody
-    public String getContacts() {
-        String res;
+    public ResponseEntity<String> listContacts() {
         try{
-            res = objectMapper.writeValueAsString(contactService.listContacts());
+            return  ResponseEntity.ok(objectMapper.writeValueAsString(contactService.listContacts()));
         }catch(JsonProcessingException ex) {
-            res = "{ err: " + ex.getMessage() + "}";
+            ///TODO log exception
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
-        return res;
     }
 
     @SuppressWarnings("SameReturnValue")
     @RequestMapping(value = "/api/contacts/{id}", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
     @ResponseBody
-    public String getContactById(@PathVariable("id") int id) {
-        String res;
+    public ResponseEntity<String> getContact(@PathVariable("id") int id) {
         try{
-            res = objectMapper.writeValueAsString(contactService.getContactById(id));
+            return ResponseEntity.ok(objectMapper.writeValueAsString(contactService.getContactById(id)));
         }catch(JsonProcessingException ex) {
-            res = "{ err: " + ex.getMessage() + "}";
+            ///TODO log exception
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
-        return res;
+    }
+
+    @SuppressWarnings("SameReturnValue")
+    @RequestMapping(value = "/api/contacts", method = RequestMethod.POST, produces = "application/json; charset=utf-8")
+    @ResponseBody
+    public ResponseEntity<String> postContact(@RequestBody String json) {
+        Contact contact;
+        try{
+            JsonNode node = objectMapper.readTree(json);
+            if(!node.has("projectId") || !node.has("contact") || !node.has("email")) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+            }
+            contact = new Contact(null, node.get("projectId").asInt(), node.get("contact").asText(), node.get("email").asText(), node.get("phone").asText());
+        }catch(Exception ex) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+        contactService.addContact(contact);
+        return ResponseEntity.status(HttpStatus.CREATED).location(URI.create("/api/projects")).body(null);
+    }
+
+    @SuppressWarnings("SameReturnValue")
+    @RequestMapping(value = "/api/contacts/{id}", method = RequestMethod.PATCH, produces = "application/json; charset=utf-8")
+    @ResponseBody
+    public ResponseEntity<String> patchContact(@PathVariable("id") int id, @RequestBody String json) {
+        Contact contact;
+        try{
+            JsonNode node = objectMapper.readTree(json);
+            if(!node.has("projectId") || !node.has("contact") || !node.has("email")) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+            }
+            if(node.has("phone")) {
+                contact = new Contact(id, node.get("projectId").asInt(), node.get("contact").asText(), node.get("email").asText(), node.get("phone").asText());
+            } else {
+                contact = new Contact(id, node.get("projectId").asInt(), node.get("contact").asText(), node.get("email").asText());
+            }
+
+        }catch(Exception ex) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+        contactService.editContact(contact);
+        return ResponseEntity.ok(json);
+    }
+
+
+    @SuppressWarnings("SameReturnValue")
+    @RequestMapping(value = "/api/contacts/{id}", method = RequestMethod.DELETE, produces = "application/json; charset=utf-8")
+    @ResponseBody
+    public ResponseEntity<String> deleteContact(@PathVariable("id") int id) {
+        contactService.deleteContact(id);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(null);
     }
 }
